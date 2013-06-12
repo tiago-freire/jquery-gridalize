216fcf33af06ede8f83cc30272679559294fdd9f
freire.tiago@gmail.com <freire.tiago@gmail.com@e2fc6f7b-9eff-afc2-af35-6afae40050be>
2013-06-12 18:57:28 +0000

    - modificando a condição de decremento da quantidade de elementos por linha para prever a óbvia situação de quando há apenas 1 elemento; - vinculando a execução do código nuclear do plugin ao evento "resize" de window, para que o plugin fique dinamicamente adaptável ao redimensionamento da janela do browser

--- jquery-gridalize/js/jquery-gridalize-0.1.js
+++ jquery-gridalize/js/jquery-gridalize-0.1.js
@@ -1,70 +1,82 @@
 jQuery.fn.extend({
-	gridalize: function(parameters) {
-                //initializing
-		var defaultOptions = {
-			verticalSpace: ''
-		};
+    gridalize: function(parameters) {
+        var run = function(rootElement, parameters) {
+            /* initializing */
+            var defaultOptions = {
+                verticalSpace: ''
+            };
 		
-		var options = jQuery.extend(defaultOptions, parameters);
-		var parent = jQuery(this);
-		var children = parent.children();
-		var totalWidth = parent.width();
-		var images = parent.children('img');
-		var countImagesLoaded = 0;
-		var calculateMarginLeft = function(totalWidth, countByLine, averageWidth) {
-			return Math.floor((totalWidth - (countByLine * averageWidth)) / (countByLine + 1));
-		};
-		var calculateRatio = function(totalWidth, countByLine, averageWidth, marginLeft) {
-                        return (totalWidth - (countByLine * (marginLeft + averageWidth) + marginLeft)) / countByLine;
-		};
+            var options = jQuery.extend(defaultOptions, parameters);
+            var parent = jQuery(rootElement);
+            var children = parent.children();
+            var totalWidth = parent.width();
+            var images = parent.children('img');
+            var countImagesLoaded = 0;
+            var calculateMarginLeft = function(totalWidth, countByLine, averageWidth) {
+                return Math.floor((totalWidth - (countByLine * averageWidth)) / (countByLine + 1));
+            };
+            var calculateRatio = function(totalWidth, countByLine, averageWidth, marginLeft) {
+                return (totalWidth - (countByLine * (marginLeft + averageWidth) + marginLeft)) / countByLine;
+            };
 
-		/* ensuring loading of images */
-		images.each(function() {
-			/* binding an increased of images count on load event */
-			jQuery(this).load(function() { countImagesLoaded++; });
+            /* ensuring loading of images */
+            images.each(function() {
+                /* binding an increased of images count on load event */
+                jQuery(this).load(function() {
+                    countImagesLoaded++;
+                });
 
-			/* when image is in cache the load event is triggered */
-			if(this.complete) {
-				jQuery(this).trigger("load");
-			}
-		});
+                /* when image is in cache the load event is triggered */
+                if(this.complete) {
+                    jQuery(this).trigger('load');
+                }
+            });
 
-		if(countImagesLoaded == images.length) {
-			var totalCount = 0;
-			var sumWidth = 0;
+            if(countImagesLoaded == images.length) {
+                var totalCount = 0;
+                var sumWidth = 0;
 			
-			children.each(function() {
-				var eachWidth = jQuery(this).outerWidth();
+                children.each(function() {
+                    var eachWidth = jQuery(this).outerWidth();
 
-				if(eachWidth > 0) {
-					jQuery(this).css('float', 'left');
-					sumWidth += eachWidth;
-					totalCount++;
-				}
-			});
+                    if(eachWidth > 0) {
+                        jQuery(this).css('float', 'left');
+                        sumWidth += eachWidth;
+                        totalCount++;
+                    }
+                });
 
-			var averageWidth = sumWidth / totalCount;
+                var averageWidth = sumWidth / totalCount;
 
-			var maxCountByLine = Math.floor(totalWidth / averageWidth);
-			var countByLine = maxCountByLine < totalCount ? maxCountByLine : totalCount;
+                var maxCountByLine = Math.floor(totalWidth / averageWidth);
+                var countByLine = maxCountByLine < totalCount ? maxCountByLine : totalCount;
 
-			var marginLeft = calculateMarginLeft(totalWidth, countByLine, averageWidth);
-                        var ratio = calculateRatio(totalWidth, countByLine, averageWidth, marginLeft);
+                var marginLeft = calculateMarginLeft(totalWidth, countByLine, averageWidth);
+                var ratio = calculateRatio(totalWidth, countByLine, averageWidth, marginLeft);
 			
-			if(ratio >= 0.5 && ratio <= 1) {
-				countByLine = Math.floor(0.75 * countByLine);
-				marginLeft = calculateMarginLeft(totalWidth, countByLine, averageWidth);
-			}
+                if(countByLine > 1 && ratio >= 0.5 && ratio <= 1) {
+                    countByLine = Math.floor(0.75 * countByLine);
+                    marginLeft = calculateMarginLeft(totalWidth, countByLine, averageWidth);
+                }
 			
-			var newProperties = {};
-			newProperties['margin-left'] = marginLeft + 'px';
-			if(options.verticalSpace) {
-				newProperties['margin-top'] = options.verticalSpace;
-				parent.css('padding-bottom', options.verticalSpace);
+                var newProperties = {};
+                newProperties['margin-left'] = marginLeft + 'px';
+                if(options.verticalSpace) {
+                    newProperties['margin-top'] = options.verticalSpace;
+                    parent.css('padding-bottom', options.verticalSpace);
 				
-			}
+                }
 
-			children.css(newProperties);
-		}
-	}
+                children.css(newProperties);
+            }
+        }
+        
+        var rootElement = this;
+        
+        jQuery(window).resize(function() {
+            run(rootElement, parameters);
+        });
+        
+        run(rootElement, parameters);
+    }
 });
